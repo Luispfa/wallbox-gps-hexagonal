7cb7f934d02eb25e5c7f0bde7db77b076596264f
Luis Flores <luispfa@gmail.com>
2023-03-12 22:07:33 +0100

    queryhandler refactored

--- src/Application/Bus/Query/AutoPilotQueryHandler.php
+++ src/Application/Bus/Query/AutoPilotQueryHandler.php
@@ -8,7 +8,6 @@ use App\Domain\Bus\Query\QueryHandler;
 use App\Domain\CoordinateX;
 use App\Domain\CoordinateY;
 use App\Domain\Direction;
-use App\Domain\ElectricVehicle;
 use App\Domain\Gps;
 use App\Domain\SpinMove;
 use App\Domain\UpperRightX;
@@ -23,22 +22,30 @@ final class AutoPilotQueryHandler implements QueryHandler
         $this->gps = $gps;
     }
 
-    public function __invoke(AutoPilotQuery $query): ElectricVehicle
+    public function __invoke(array $queries): array
     {
-        $upperRightX = new UpperRightX($query->upperRightX());
-        $upperRightY = new UpperRightY($query->upperRightY());
-        $coordinateX = new CoordinateX($query->coordinateX(), $query->upperRightX());
-        $coordinateY = new CoordinateY($query->coordinateY(), $query->upperRightY());
-        $direction = new Direction($query->direction());
-        $spinMove = new SpinMove($query->spinMove());
+        $electricVehicles = [];
 
-        return $this->gps->__invoke(
-            $upperRightX,
-            $upperRightY,
-            $coordinateX,
-            $coordinateY,
-            $direction,
-            $spinMove
-        );
+        foreach ($queries as $query) {
+            $upperRightX = new UpperRightX($query->upperRightX());
+            $upperRightY = new UpperRightY($query->upperRightY());
+            $coordinateX = new CoordinateX($query->coordinateX(), $query->upperRightX());
+            $coordinateY = new CoordinateY($query->coordinateY(), $query->upperRightY());
+            $direction = new Direction($query->direction());
+            $spinMove = new SpinMove($query->spinMove());
+
+            $electricVehicle = $this->gps->__invoke(
+                $upperRightX,
+                $upperRightY,
+                $coordinateX,
+                $coordinateY,
+                $direction,
+                $spinMove
+            );
+
+            $electricVehicles[] = $electricVehicle;
+        }
+
+        return $electricVehicles;
     }
 }
